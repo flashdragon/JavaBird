8049af25737b132620b8c01986931cba981e107f
flashdragon <yooh3574@naver.com>
2024-02-20 16:41:17 +0900

    자바버드 첫시작

---
+++ src/main/java/bird/JavaBird/JavaBirdApplication.java
@@ -0,0 +1,13 @@
+package bird.JavaBird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBirdApplication.class, args);
+	}
+
+}
