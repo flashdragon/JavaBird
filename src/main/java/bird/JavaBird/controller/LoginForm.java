4e967bbdb3fb6854194f4c7b09e1aa7d567dcf88
flashdragon <yooh3574@naver.com>
2024-02-20 19:37:03 +0900

    로그인홈 화면추가

---
+++ src/main/java/bird/JavaBird/controller/LoginForm.java
@@ -0,0 +1,10 @@
+package bird.JavaBird.controller;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    private String memberName;
+    private String password;
+}
---
+++ src/main/java/bird/JavaBird/domain/Member.java
@@ -0,0 +1,10 @@
+package bird.JavaBird.domain;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String memberName;
+    private String password;
+}
